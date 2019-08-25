c1f9358d83389eca8f9d71a8277b0972b9fac4b2
Sree K <grok.ksl@gmail.com>
2019-08-26 02:02:16 +0530

    small build

---
+++ build/precache-manifest.e582a167b15753370a1a53ff33986280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2010331544ad8157aa11852d41f79e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049d2b7c7319658479b0",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d8824adaaf494b0db3c8",
+    "url": "/static/css/main.cede614b.chunk.css"
+  },
+  {
+    "revision": "049d2b7c7319658479b0",
+    "url": "/static/js/2.cf59a0ca.chunk.js"
+  },
+  {
+    "revision": "d8824adaaf494b0db3c8",
+    "url": "/static/js/main.69215926.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
